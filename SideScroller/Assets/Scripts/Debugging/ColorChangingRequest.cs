96280882256072e1ab898b0c31f3d9f43967ae0b
torn2706 <torn0627@gmail.com>
2023-07-29 16:55:33 +0200

    Color Changing System

---
+++ SideScroller/Assets/Scripts/Debugging/ColorChangingRequest.cs
@@ -0,0 +1,10 @@
+using Unity.Entities;
+
+namespace TIC.FunnyStarts
+{
+    public class ColorChangingRequest : IComponentData
+    {
+        public ColorName colorName;
+    }
+}
+
--- SideScroller/Assets/Scripts/Debugging/DebugSystem.cs
+++ SideScroller/Assets/Scripts/Debugging/DebugSystem.cs
@@ -6,11 +6,24 @@ using UnityEngine;
 
 public partial class DebugSystem : SystemBase
 {
+    protected override void OnStartRunning()
+    {
+        //DebugOnStart();
+    }
+
     protected override void OnUpdate()
     {
         foreach (var reloadRequest in SystemAPI.Query<ReloadRequest>()) 
         {
             //Debug.Log(reloadRequest.playerEntity);  
         }
+    }
+
+    private void DebugOnStart()
+    {
+        Entity newEntity = EntityManager.CreateEntity();
+        ColorChangingRequest request = new ColorChangingRequest() { colorName = ColorName.Air };
+        EntityManager.AddComponentData(newEntity, request);
+        EntityManager.AddComponent<RequestTag>(newEntity);
     }
 }
